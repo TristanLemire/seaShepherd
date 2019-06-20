05e074f6d5c2b5ea0fba9590121c72ece4c26dea
edwinvautier <edwin.vautier@gmail.com>
2019-06-20 09:39:47 +0200

    knex + router

--- server/index.js
+++ server/index.js
@@ -2,13 +2,14 @@ const express = require('express')
 const consola = require('consola')
 const { Nuxt, Builder } = require('nuxt')
 const app = express()
+const apiRouter = require('./apiRouter');
 
 // Import and Set Nuxt.js options
 let config = require('../nuxt.config.js')
 config.dev = !(process.env.NODE_ENV === 'production')
 
 async function start() {
-  // Init Nuxt.js
+  // Init Nuxt.js 
   const nuxt = new Nuxt(config)
 
   const { host, port } = nuxt.options.server
@@ -22,10 +23,11 @@ async function start() {
   }
 
   // Give nuxt middleware to express
+  app.use('/api', apiRouter);
   app.use(nuxt.render)
 
   // Listen the server
-  app.listen(port, host)
+  app.listen(port, host);
   consola.ready({
     message: `Server listening on http://${host}:${port}`,
     badge: true
