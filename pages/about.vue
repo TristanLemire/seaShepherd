c977425c159037b453e02ce0b98b0204ae71d909
TristanLemire <tristan.lemire.sio@gmail.com>
2019-06-20 17:38:34 +0200

    start inté of transition

--- pages/about.vue
+++ pages/about.vue
@@ -2,6 +2,7 @@
 <div>
   <video src="../assets/video/home.mp4" class="home__video" autoplay></video>
         <SoundButton />
+              <SkipButton/>
 </div>
 </template>
 <script>
@@ -9,7 +10,9 @@ import Menu from "~/components/Menu.vue";
 import SkipButton from "~/components/SkipButton.vue";
 import SoundButton from "~/components/SoundButton.vue";
 
+
 export default {
+  transition: "intro",
   components: {
     Menu,
     SkipButton,
@@ -37,3 +40,89 @@ export default {
   }
 };
 </script>
+
+<style lang="scss">
+$t-duration: 800ms;
+$t-delay: 300ms;
+
+.intro-enter-active,
+.intro-leave-active {
+  transition-duration: $t-duration * 2;
+
+  &::before,
+  &::after {
+    content: "";
+    position: fixed;
+    top: 0;
+    left: 0;
+    z-index: 2;
+    display: block;
+    width: 100%;
+    height: 50%;
+    transition-property: opacity, transform;
+    transition-timing-function: ease-in-out;
+  }
+
+  &::before {
+    background-color: #2e2e2e;
+  }
+
+  &::after {
+    top: 50%;
+    background-color: #2e2e2e;
+  }
+}
+
+.intro-leave {
+  &::before,
+  &::after {
+    transform: scaleX(0);
+  }
+}
+
+.intro-leave-active {
+  &::before {
+    transition-duration: $t-duration;
+  }
+
+  &::after {
+    transition-duration: $t-duration - $t-delay;
+    transition-delay: $t-delay;
+  }
+}
+
+.intro-leave-to {
+  &::before,
+  &::after {
+    transform: scale(1);
+    transform-origin: left;
+  }
+}
+
+.intro-enter {
+  &::before,
+  &::after {
+    transform: scaleX(1);
+  }
+}
+
+.intro-enter-active {
+  &::before {
+    transition-duration: $t-duration;
+  }
+
+  &::after {
+    transition-duration: $t-duration - $t-delay;
+    transition-delay: $t-delay;
+  }
+}
+
+.intro-enter-to {
+  &::before,
+  &::after {
+    transform: scaleX(0);
+    transform-origin: right;
+  }
+}
+</style>
+
