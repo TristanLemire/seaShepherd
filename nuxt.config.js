7c6dd401e3e4f731e0f6333cd5c02e0cb8e099b5
edwinvautier <edwin.vautier@gmail.com>
2019-06-25 11:07:47 +0200

    conflicts solved

--- nuxt.config.js
+++ nuxt.config.js
@@ -44,7 +44,6 @@ module.exports = {
     // Doc:https://github.com/nuxt-community/modules/tree/master/packages/bulma
     '@nuxtjs/bulma',
     '@nuxtjs/axios',
-    'fullpage-nuxt',
   ],
   /*
   ** Build configuration
