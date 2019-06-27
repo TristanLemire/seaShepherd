c7a2e318e51dfab21bf6a5f46f213bbd57929cd1
edwinvautier <edwin.vautier@gmail.com>
2019-06-27 10:30:00 +0200

    Importing Tristan's code

---
+++ components/ScrollDown.vue
@@ -0,0 +1,87 @@
+<template>
+  <div class="scroll-down">
+    <span class="left">Scroll</span>
+    <span class="mouse"></span>
+    <span class="right">Down</span>
+  </div>
+</template>
+
+<script>
+  if (process.client) {
+    let scroll = document.querySelector('.scroll-down');
+
+    window.addEventListener('scroll', function () {
+      setTimeout(function() {
+        scroll.style.opacity = 0;
+      }, 100);
+      setTimeout(function() {
+        scroll.style.opacity = 1;
+      }, 500)
+    })
+  }
+</script>
+
+
+<style lang="scss">
+  $dark-gray: #fff;
+  $mouse-width: 12px;
+
+  .scroll-down {
+    position: fixed;
+    left: 50%;
+    top: 94%;
+    transform: translate(-50%,-94%);
+    transition: opacity .25s ease-in-out;
+  }
+
+  .mouse {
+    display: inline-block;
+    vertical-align: middle;
+    width: $mouse-width;
+    height: 22px;
+    position: relative;
+    border: 2px solid $dark-gray;
+    border-radius: $mouse-width;
+    
+    &:before {
+      content: '';
+      display: inline-block;
+      width: 2px;
+      height: 6px;
+      position: absolute;
+      top: 5px;
+      left: 3px;
+      background-color: $dark-gray;
+      animation: mouse-animation 2.75s ease-in-out infinite;
+    }
+  }
+
+  @keyframes mouse-animation {
+    0% {
+    transform: scale(1,0);
+    transform-origin: 50% 0;
+    }
+    20%, 80% {
+    transform: scale(1,1);
+    }
+    81% {
+    transform-origin: 50% 100%;  
+    }
+    100% {
+    transform: scale(1,0);
+    transform-origin: 50% 100%; 
+    }
+  }
+
+  .left,
+  .right {
+    display: inline-block;
+    vertical-align: middle;
+    padding: 0 18px;
+    font-size: 13px;
+    font-weight: bold;
+    color: $dark-gray;
+    text-transform: uppercase;
+    font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Helvetica, Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol";
+  }
+</style>
--- pages/steps/_id.vue
+++ pages/steps/_id.vue
@@ -19,14 +19,13 @@
     </section>
     <section v-for="content in contents" :key="content.id" :class="'scrollto ' + content.type">
       <video
+        data-play="false"
         v-if="content.type === 'video'"
         class="home__video"
         :src="'/'+content.source"
-        @click="controlVideo"
       ></video>
       <svg
         v-if="content.type === 'video'"
-        @click="controlVideo"
         class="play"
         width="100"
         height="100"
@@ -39,7 +38,6 @@
       </svg>
       <svg
         v-if="content.type === 'video'"
-        @click="controlVideo"
         display="none"
         class="play"
         width="100"
@@ -72,8 +70,9 @@
         <p v-if="content.type === 'text' && content.order !== 0">{{ content.content }}</p>
       </div>
     </section>
+    <ScrollDown/>
     <!-- <a :href="'/steps/'+step.next" class="button">Next step</a> -->
-    <Footer class="scrollto" :next="'/steps/'+step.next" />
+    <Footer class="scrollto" :next="'/steps/'+step.next"/>
   </div>
 </template>
 
@@ -84,32 +83,153 @@ import Logo from "~/components/Logo.vue";
 import Back from "~/components/Back.vue";
 import { returnStatement } from "babel-types";
 import Footer from "~/components/Footer.vue";
+import ScrollDown from "~/components/ScrollDown.vue";
 
 if (process.client) {
+  function videoControl(videos, svgPlays, svgPauses, i) {
+    let video = videos[i];
+    let svgPlay = svgPlays[i];
+    let svgPause = svgPauses[i];
+    let audioSound = document.querySelector("audio");
 
-  // TODO: solve scroll problems 
+    if (video.getAttribute("data-play") == "false") {
+      for (let i2 = 0; i2 < videos.length; i2++) {
+        if (videos[i2].getAttribute("data-play") == "true") {
+          videos[i2].setAttribute("data-play", "false");
+          videos[i2].pause();
+          svgPauses[i2].setAttribute("display", "none");
+          svgPlays[i2].setAttribute("display", "");
+        }
+      }
+      video.play();
+      video.setAttribute("data-play", "true");
+      svgPause.style.opacity = 1;
+      svgPlay.setAttribute("display", "none");
+      svgPause.setAttribute("display", "");
+      audioSound.pause();
+      setTimeout(function() {
+        svgPause.style.opacity = 0;
+      }, 200);
+    } else {
+      video.pause();
+      video.setAttribute("data-play", "false");
+      svgPause.setAttribute("display", "none");
+      svgPlay.setAttribute("display", "");
+      if (localStorage.getItem("sound") == "ON") {
+        audioSound.play();
+      }
+    }
+  }
+  setTimeout(() => {
+    let videos = document.querySelectorAll("video");
+    let svgPlays = document.querySelectorAll(".video svg:nth-child(2)");
+    let svgPauses = document.querySelectorAll(".video svg:nth-child(3)");
+    let audioSound = document.querySelector("audio");
+
+    window.addEventListener("scroll", () => {
+      for (let i = 0; i < videos.length; i++) {
+        videos[i].setAttribute("data-play", "false");
+        videos[i].pause();
+        svgPauses[i].setAttribute("display", "none");
+        svgPlays[i].setAttribute("display", "");
+        if (localStorage.getItem("sound") != "OFF") {
+          audioSound.play();
+        }
+      }
+    });
+
+    let allSection = document.querySelectorAll("section");
+    for (let i = 0; i < allSection.length; i++) {
+      console.log(allSection[i]);
+      let waypoint = new Waypoint({
+        element: allSection[i],
+        handler: function(direction) {
+          if (allSection[i].className == "scrollto video") {
+            let laVideo = allSection[i].querySelector("video");
+            let leSvgPlay = allSection[i].querySelector("svg:nth-child(2)");
+            let leSvgPause = allSection[i].querySelector("svg:nth-child(3)");
+            laVideo.play();
+            console.log(allSection[i]);
+            laVideo.setAttribute("data-play", "true");
+            leSvgPause.style.opacity = 1;
+            leSvgPlay.setAttribute("display", "none");
+            leSvgPause.setAttribute("display", "");
+            audioSound.pause();
+            setTimeout(function() {
+              leSvgPause.style.opacity = 0;
+            }, 200);
+          }
+        },
+        offset: "0%"
+      });
+    }
+
+    // for (let i = 0; i < videos.length; i++) {
+    //   videos[i].setAttribute("id", i);
+    // }
+
+    // for (let i = 0; i < videos.length; i++) {
+    //   let theVideo = videos[i];
+    //   let waypoint = new Waypoint({
+    //   element: theVideo,
+    //   handler: function(direction) {
+    //     theVideo.play();
+    //     svgPauses[i].style.opacity = 1;
+    //     svgPlays[i].setAttribute("display", "none");
+    //     svgPauses[i].setAttribute("display", "");
+    //     audioSound.pause();
+    //     setTimeout(function() {
+    //       svgPauses[i].style.opacity = 0;
+    //     }, 200);
+    //     for (let i2 = 0; i2 < videos.length; i2++) {
+    //       if(videos[i2] != theVideo){
+    //         videos[i2].pause();
+    //         svgPauses[i2].setAttribute("display", "none");
+    //         svgPlays[i2].setAttribute("display", "");
+    //       }
+    //     }
+    //   },
+    //   offset: '0%'
+    // });
+
+    // }
+
+    for (let i = 0; i < videos.length; i++) {
+      videos[i].addEventListener("click", () => {
+        videoControl(videos, svgPlays, svgPauses, i);
+      });
+      svgPlays[i].addEventListener("click", () => {
+        videoControl(videos, svgPlays, svgPauses, i);
+      });
+      svgPauses[i].addEventListener("click", () => {
+        videoControl(videos, svgPlays, svgPauses, i);
+      });
+    }
+  }, 200);
+
+  // TODO: solve scroll problems
   setTimeout(() => {
     let sections = document.querySelectorAll(".scrollto");
     let arr = [];
     sections.forEach(section => {
-      arr.push(section)
-    })
-    if (sections[1].className === 'scrollto text')arr.splice(1,1);
-    sections = arr
+      arr.push(section);
+    });
+    if (sections[1].className === "scrollto text") arr.splice(1, 1);
+    sections = arr;
     let pos = 0;
     let scroll = 0;
     let flag = true;
     let offset = 0;
-    
+
     window.addEventListener("wheel", e => {
       // If the function is allowed to run
       if (flag) {
         if (e.deltaY > 5 && pos < sections.length - 1) {
           pos += 1;
           offset = sections[pos].offsetTop;
-          
+
           window.scrollTo({
-            behavior: 'smooth',
+            behavior: "smooth",
             top: offset
           });
           flag = false;
@@ -121,9 +241,9 @@ if (process.client) {
         } else if (e.deltaY < -5 && pos > 0) {
           pos -= 1;
           offset = sections[pos].offsetTop;
-          
+
           window.scrollTo({
-            behavior: 'smooth',
+            behavior: "smooth",
             top: offset
           });
           flag = false;
@@ -149,13 +269,16 @@ if (process.client) {
 let memo;
 
 export default {
-  /* head() {
+  head() {
     return {
       script: [
-        { src: "https://cdnjs.cloudflare.com/ajax/libs/scrollify/1.0.19/jquery.scrollify.min.js" },
+        {
+          src:
+            "https://cdnjs.cloudflare.com/ajax/libs/waypoints/4.0.1/noframework.waypoints.min.js"
+        }
       ]
     };
-  }, */
+  },
 
   data() {
     return {
@@ -167,7 +290,8 @@ export default {
     Logo,
     StepsMenu,
     Back,
-    Footer
+    Footer,
+    ScrollDown
   },
   asyncData({ params }) {
     return fetch("http://localhost:3000/api/steps/" + params.id, {
@@ -198,53 +322,50 @@ export default {
       let active = localStorage.getItem("sound");
       let audios = document.querySelectorAll("audio");
       if (active == "OFF" || active == null) {
-        if (video != null) {
-          video.volume = 0;
-        }
         if (audios.length > 0) {
           audios.forEach(audio => {
             audio.pause();
           });
         }
       }
-    },
-
-    controlVideo() {
-      let video = document.querySelector("video");
-      let svgPlay = document.querySelector(".video svg:nth-child(2)");
-      let svgPause = document.querySelector(".video svg:nth-child(3)");
-      let audioSound = document.querySelector("audio");
-
-      if (memo === false) {
-        memo = true;
-        svgPause.setAttribute("display", "none");
-        svgPlay.setAttribute("display", "");
-        video.pause();
-        if (localStorage.getItem("sound") == "ON") {
-          audioSound.play();
-        }
-      } else if (memo === true) {
-        svgPause.style.opacity = 1;
-        memo = false;
-        svgPlay.setAttribute("display", "none");
-        svgPause.setAttribute("display", "");
-        setTimeout(function() {
-          svgPause.style.opacity = 0;
-        }, 200);
-        video.play();
-        audioSound.pause();
-      } else {
-        memo = false;
-        video.play();
-        audioSound.pause();
-        svgPause.style.opacity = 1;
-        svgPlay.setAttribute("display", "none");
-        svgPause.setAttribute("display", "");
-        setTimeout(function() {
-          svgPause.style.opacity = 0;
-        }, 200);
-      }
     }
+
+    // controlVideo() {
+    //   let video = document.querySelector("video");
+    //   let svgPlay = document.querySelector(".video svg:nth-child(2)");
+    //   let svgPause = document.querySelector(".video svg:nth-child(3)");
+    //   let audioSound = document.querySelector("audio");
+
+    //   if (memo === false) {
+    //     memo = true;
+    //     svgPause.setAttribute("display", "none");
+    //     svgPlay.setAttribute("display", "");
+    //     video.pause();
+    //     if (localStorage.getItem("sound") == "ON") {
+    //       audioSound.play();
+    //     }
+    //   } else if (memo === true) {
+    //     svgPause.style.opacity = 1;
+    //     memo = false;
+    //     svgPlay.setAttribute("display", "none");
+    //     svgPause.setAttribute("display", "");
+    //     setTimeout(function() {
+    //       svgPause.style.opacity = 0;
+    //     }, 200);
+    //     video.play();
+    //     audioSound.pause();
+    //   } else {
+    //     memo = false;
+    //     video.play();
+    //     audioSound.pause();
+    //     svgPause.style.opacity = 1;
+    //     svgPlay.setAttribute("display", "none");
+    //     svgPause.setAttribute("display", "");
+    //     setTimeout(function() {
+    //       svgPause.style.opacity = 0;
+    //     }, 200);
+    //   }
+    // }
   },
   beforeMount() {
     this.soundActive();
